363cf716e383f1910322f78c3e52355af18f0851
s-kumar72 <kumarsadhana75@gmail.com>
2022-04-24 11:27:11 -0500

    fix error with PollsConfig

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
+# empty string signifies home page
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,14 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def store(request):
+    context = {}
+    return render(request, 'polls/store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'polls/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'polls/checkout.html', context)
