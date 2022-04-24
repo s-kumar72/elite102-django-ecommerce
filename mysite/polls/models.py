8ab7bb3a3b3a4efb3bf0d649d8015f7f1d0e8900
s-kumar72 <kumarsadhana75@gmail.com>
2022-04-24 15:32:49 -0500

    make migrations

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -15,7 +15,8 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
-    digital = models.BooleanField(default=False, null=True, blank=False)
+    digital = models.BooleanField(default=False, null=True, blank=True)
+    # image
 
     def __str__(self):
         return self.name
@@ -28,3 +29,21 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quanitity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.address
