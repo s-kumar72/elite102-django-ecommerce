2dc5818bad96bf8118d016b3a4b30ea7d073f8e8
s-kumar72 <kumarsadhana75@gmail.com>
2022-04-24 18:37:48 -0500

    fix error with .data attribute

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -32,7 +32,7 @@ def checkout(request):
     return render(request, 'polls/checkout.html', context)
 
 def updateItem(request):
-    data = json.loads(request.data)
+    data = json.loads(request.body)
     productId = data['productId']
     action = data['action']
 
