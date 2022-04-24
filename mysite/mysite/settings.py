e7b35b87f0ee33c3f777642aaefb8ba34df2f630
s-kumar72 <kumarsadhana75@gmail.com>
2022-04-24 11:35:36 -0500

    add css & images static files

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# BASE_DIR takes it back to root directory
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
