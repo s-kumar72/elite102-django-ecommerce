3303629af561536780ba41867904b35e6b1ed06d
s-kumar72 <kumarsadhana75@gmail.com>
2022-04-24 17:55:32 -0500

    update to add to cart

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 from django.http import JsonResponse
+import json
 
 def store(request):
     products = Product.objects.all()
@@ -31,4 +32,27 @@ def checkout(request):
     return render(request, 'polls/checkout.html', context)
 
 def updateItem(request):
+    data = json.loads(request.data)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action: ', action)
+    print('Product: ', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
     return JsonResponse('Item was added', safe=False)
